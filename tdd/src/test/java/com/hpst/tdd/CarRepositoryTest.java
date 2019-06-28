338369a00264d39d08aca60a8f5af2c386a6b06e
Harpal Singh <harpal.rtu@gmail.com>
2019-06-28 16:18:59 +0530

    when verify then

--- tdd/src/test/java/com/hpst/tdd/CarRepositoryTest.java
+++ tdd/src/test/java/com/hpst/tdd/CarRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.hpst.tdd;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.hpst.tdd.repository.CarRepository;
+import com.hpst.tdd.service.CarService;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -18,6 +24,8 @@ public class CarRepositoryTest {
 	@Mock
 	CarRepository carRepository;
 	
+	@Mock
+	CarService carService;
 	/**
 	 * 
 	 * No need to test jpa crud repository methods, this is just to show you to test crud repository interactions
@@ -27,14 +35,20 @@ public class CarRepositoryTest {
 	public void test_FindCar() {
 		
 		//arrange 
-		given(carRepository.findByName(anyString())).willReturn(new Car(1,"farari","hybrid"));
-		
-     	//act
-		carRepository.findByName("farari");
-		
-		//assertion
-		then(carRepository).should().findByName("farari");
+		Car inputCar = mock(Car.class);
+		//for given condition , it should always return inputCar
+		given(carRepository.findByName(anyString())).willReturn(inputCar);
 	
-	
+		//act 
+		//call method on mocked object
+		Car expectedCar = carRepository.findByName("farari");
+		
+		//verify behavior of mocked object post act
+		//for example check if mockedObject carRepository have been called twice 
+		verify(carRepository , times(1)).findByName("farari");
+		verify(carRepository , never()).findAll();
+		
+		assertEquals(expectedCar , inputCar);
+		
 	}
 }
